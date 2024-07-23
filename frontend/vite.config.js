401ce05b3c7b17fe30b36534ce653be65ebbbb93
AlphaAbdullah7 <muhammadabdullah932747@gmail.com>
2024-07-24 02:27:43 +0500

    A

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "index.html", // Ensure this matches your deployment configuration
+	},
 	server: {
 		port: 3000,
 		proxy: {
